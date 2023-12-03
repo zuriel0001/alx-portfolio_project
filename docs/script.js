806e4ce3f0ee0b20032298ed3c3762d9d8d2fc64
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-12-03 00:14:17 +0000

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,4 @@
+function toggleAboutSection() {
+    var aboutSection = document.getElementById('about-section');
+    aboutSection.style.display = (aboutSection.style.display === 'none' || aboutSection.style.display === '') ? 'block' : 'none';
+}
