995c45f0d6b8717afb2210e3d4556193adee0191
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-12-03 22:38:33 +0000

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,9 @@
-function toggleAboutSection() {
-    var aboutSection = document.getElementById('about-section');
-    aboutSection.style.display = (aboutSection.style.display === 'none' || aboutSection.style.display === '') ? 'block' : 'none';
+function toggleAboutSection(sectionId) {
+    var aboutSection = document.getElementById(sectionId);
+    aboutSection.style.display = (aboutSection.style.display === 'none' || aboutSection.style.display === '') ? 'flex' : 'none';
+}
+
+function toggleAboutUsSection(sectionId) {
+    var aboutUsSection = document.getElementById(sectionId);
+    aboutUsSection.style.display = (aboutUsSection.style.display === 'none' || aboutUsSection.style.display === '') ? 'flex' : 'none';
 }
